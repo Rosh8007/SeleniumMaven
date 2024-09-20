295de46807e65e43babd3835655653e5acc1498e
Rosh8007 <roshanaware890@gmail.com>
2024-09-20 19:09:34 +0530

    Minor updatess

--- src/test/java/Actions/Doubleclick.java
+++ src/test/java/Actions/Doubleclick.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -26,7 +25,6 @@ public class Doubleclick {
 
 		Actions ac=new Actions(driver);
 		ac.doubleClick(clickEl).build().perform();
-
 
 		driver.quit();
 	}
