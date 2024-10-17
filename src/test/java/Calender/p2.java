9d0e231e4fb9ca4369a558b5dd5795b7022d6eb9
Rosh8007 <roshanaware890@gmail.com>
2024-10-17 14:44:50 +0530

    Adding minor updates

--- src/test/java/Calender/p2.java
+++ src/test/java/Calender/p2.java
@@ -1,9 +1,6 @@
 package Calender;
 
-import java.util.Iterator;
 import java.util.List;
-
-import javax.xml.xpath.XPath;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,42 +10,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class p2 {
-	
+
 	public static void main(String[] args) {
-		
+
 		WebDriverManager.chromedriver().setup();
-		
+
 		WebDriver driver=new ChromeDriver();
-		
+
 		driver.get("https://testautomationpractice.blogspot.com/");
-		
+
 		driver.manage().window().maximize();
-		
+
 		driver.findElement(By.id("datepicker")).click();
-		
+
 		String  text=driver.findElement(By.xpath("//*[@class='ui-datepicker-title']")).getText();
-	
+
 		System.out.println(text);
-		
+
 		while(text.equalsIgnoreCase("December 2024")) 
 		{
 			driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/div/a[2]/span")).click();
 		}
-		
-		 List<WebElement> days=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//tbody//tr//td"));
-		
+
+		List<WebElement> days=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//tbody//tr//td"));
+
 		for(WebElement dy:days) 
 		{
-			
-			if (dy.getText().equals("22")) {
-				
+			if (dy.getText().equals("2")) {
+
 				System.out.println(dy.getText());
 				dy.click();
 			}
-			
+
 		}
-		
-	
+
+
 	}
 
 }
